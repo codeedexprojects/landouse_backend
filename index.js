04cfe6f1bf27d8d40b97058ee5b29c2459381058
Kajahussain123 <kajahusain513@gmail.com>
2025-05-01 10:15:23 +0530

    updates

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-const corsOptions = {
-    origin: [
-      'http://localhost:5173/',      
-      'http://localhost:5174/',
-      'https://landouse.netlify.app/'
-
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // if you're using cookies or auth headers
-  };
-  
-  app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "500mb" }));  
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
